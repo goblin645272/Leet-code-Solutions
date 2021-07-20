ee62080c93753d29635b67a553bcfccbb4cb3cac
goblin645272 <75854892+goblin645272@users.noreply.github.com>
2021-07-20 19:58:50 +0530

    Add Two numbers
    You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
    
    You may assume the two numbers do not contain any leading zero, except the number 0 itself.
    
     https://leetcode.com/problems/add-two-numbers/

---
+++ QNO 2.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        list = None
+        temp = None
+        s1 = ""
+        s2 = ""
+        while l1 is not None or l2 is not None:
+            if l1 is not None:
+                s1 = str(l1.val) + s1
+                l1 = l1.next
+
+            if l2 is not None:
+                s2 = str(l2.val) + s2
+                l2 = l2.next
+
+        res = str(int(s1) + int(s2))
+
+        for i in range(1, len(res) + 1, 1):
+            if temp is None:
+                temp = list = ListNode(int(res[-i]))
+            else:
+                temp.next = ListNode(int(res[-i]))
+                temp = temp.next
+
+        return list   
